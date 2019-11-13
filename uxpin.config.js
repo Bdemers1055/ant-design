14f0111b6f47daa304790ee6709be16f2abc2d88
bdemers1055 <bdemers1055@gmail.com>
2019-11-13 09:33:17 -0500

    add uxpin config file

---
+++ uxpin.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    components: {
+      categories: [
+        {
+          name: 'General',
+          include: [
+            '../node_modules/antd/es/button/button.js',
+          ]
+        }
+      ]
+    },
+    name: 'Antd Design System'
+  };
