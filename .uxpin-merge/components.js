f898bad45f1663832f2c2feef8dc6f0cc7b7e6eb
bdemers1055 <bdemers1055@gmail.com>
2019-11-14 10:19:26 -0500

    trying to add button in experimental mode with new package

--- .uxpin-merge/components.js
+++ .uxpin-merge/components.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Button from '../components/button/button';
-import Wrapper from '../components/button/button.tsx';
 export {
   Button,
-  Wrapper,
   React,
   ReactDOM,
 };
