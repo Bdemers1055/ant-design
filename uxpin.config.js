92cd7f646061770f1aafc0aab4cac2e0d87dfb20
bdemers1055 <bdemers1055@gmail.com>
2019-11-22 16:16:28 -0500

    these changes to link to button.jsx file broke the build

--- uxpin.config.js
+++ uxpin.config.js
@@ -4,7 +4,7 @@ module.exports = {
         {
           name: 'General',
           include: [
-            './components/button/presets/default.jsx',
+            './components/button/button.jsx',
           ]
         }
       ]
